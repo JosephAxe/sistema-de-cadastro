9e12d57cb763d89034f644efdcd22c1d19034fa2
JosephAxe <95534308+JosephAxe@users.noreply.github.com>
2023-01-23 14:01:44 -0300

    Voltando pra última modificação

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 console.log("Rodando o sistema de gerenciamentos de jogos")
 console.log("Aplicação esta se conectando ao banco de dados")
 //mais um console log de teste:
-console.log("O Paulo fez esse commit sem você ver na aula")
-console.log("Enviando para o GitHub")
+console.log("O Paulo fez esse commit sem você ver na aula"
