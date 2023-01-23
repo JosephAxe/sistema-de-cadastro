eee833b2b78609f18062cf03d6936fa30e0cc3bb
José Machado <95534308+JosephAxe@users.noreply.github.com>
2023-01-23 13:36:37 -0300

    adicionando um novo console.log

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamentos de jogos")
 console.log("Aplicação esta se conectando ao banco de dados")
+//mais um console log de teste:
+console.log("O Paulo fez esse commit sem você ver na aula"
